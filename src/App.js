26913486785717d7673cb95cc02c257c14517ff3
monaj <mohammednajeem18@gmail.com>
2023-05-18 11:12:22 +0100

    buiding home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import home from './component.jsx/home';
+import Home from './component.jsx/home';
 
 function App() {
   return (<div>
-    hi
+    <Home />
   </div>
   );
 }
