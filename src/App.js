2f45cdce187016f97e9bc8d7c2b8b26531fe894d
monaj <mohammednajeem18@gmail.com>
2023-05-18 10:49:17 +0100

    creating a new home

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import home from './component.jsx/home';
+
+function App() {
+  return (<div>
+    hi
+  </div>
+  );
+}
+
+export default App;
---
+++ src/component.jsx/home.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+const home = () => {
+    return ( <> this is home</> );
+}
+ 
+export default home;
